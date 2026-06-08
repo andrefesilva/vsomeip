a3a5816bd96122bb32490156bcbbcf8a8c798e92
joelpinto <joel.pinto@ctw.bmwgroup.com>
2026-06-08 13:20:45 +0100

    Optimize restart routing test
    Optimize restart_routing_test.
    Debug builds inherits an expected performance hit that had impact on slow and time sensitives tests.
    This PR optimizes the restart routing tests by running all in a single address space, no process spawning
    is required for this test case, this bring roughly a 10s reduction on each test fixture (debug build with parallel
    execution).
    Moreover, a duplicated test is removed.

--- test/network_tests/restart_routing_tests/restart_routing_test_globals.hpp
+++ test/network_tests/restart_routing_tests/restart_routing_test_globals.hpp
@@ -5,28 +5,7 @@
 
 #pragma once
 
-#include <array>
-#include "../someip_test_utils.hpp"
+#include <inttypes.h>
 
 // Number of service consumers to be created
-#define NUM_SERVICE_CONSUMERS 4
-
-namespace restart_routing {
-
-/**
- * Interprocess synchronization data.
- */
-struct restart_routing_test_interprocess_sync {
-    enum registration_status { STATE_REGISTERED = 0x00, STATE_NOT_REGISTERED = 0x01, STATE_DEREGISTERED = 0x02 };
-    enum sending_status { SEND_MESSAGES = 0x00, WAITING_TO_SEND_MESSAGES = 0x01 };
-
-    boost::interprocess::interprocess_mutex client_mutex_;
-    boost::interprocess::interprocess_condition client_cv_;
-
-    std::array<registration_status, NUM_SERVICE_CONSUMERS> client_status_ = {
-            registration_status::STATE_NOT_REGISTERED, registration_status::STATE_NOT_REGISTERED, registration_status::STATE_NOT_REGISTERED,
-            registration_status::STATE_NOT_REGISTERED};
-    sending_status sending_status_ = {sending_status::WAITING_TO_SEND_MESSAGES};
-};
-
-}
+static constexpr uint32_t NUM_SERVICE_CONSUMERS{4};
