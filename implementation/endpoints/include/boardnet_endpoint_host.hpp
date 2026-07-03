42e2c25fba44ffaf5084794ae3d0f29ed28755b9
ricardolleite <ricardo.l.leite@bmw.de>
2026-07-03 16:10:12 +0200

    endpoints: drop unneeded errors
    Drop E_MALFORMED_MESSAGE when usei receives bad length.
    Drop E_MALFORMED_MESSAGE in the magic cookie logic. It's neither
    necessary nor specified anywhere

--- implementation/endpoints/include/boardnet_endpoint_host.hpp
+++ implementation/endpoints/include/boardnet_endpoint_host.hpp
@@ -32,8 +32,6 @@ public:
     virtual void on_disconnect(std::shared_ptr<boardnet_endpoint> _endpoint) = 0;
     virtual bool on_bind_error(std::shared_ptr<boardnet_endpoint> _endpoint, const boost::asio::ip::address& _remote_address,
                                uint16_t _remote_port, uint16_t& _local_port) = 0;
-    virtual void on_error(const byte_t* _data, length_t _length, boardnet_endpoint* const _receiver,
-                          const boost::asio::ip::address& _remote_address, std::uint16_t _remote_port) = 0;
     virtual client_t get_client() const = 0;
     virtual std::string get_client_host() const = 0;
     virtual instance_t find_instance(service_t _service, boardnet_endpoint* const _endpoint) const = 0;
